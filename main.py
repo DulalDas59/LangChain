aa4c1603e082e33fbc283f296207f416317498e0
dulaldas5 <dulaldas5990@gmail.com>
2026-01-11 22:03:02 +0530

    tools code

---
+++ main.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from get_weather import get_weather
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    get_weather("Bengaluru")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
